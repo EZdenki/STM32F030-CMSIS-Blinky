59f2c2f5628223679a5a558859a3d6bddce2b983
Mike <sandynomike@gmail.com>
2023-08-29 14:50:49 +0900

    Updated comments

--- main.c
+++ main.c
@@ -16,20 +16,20 @@
 //    Wire an LED and 1k resistor between pin 6 and ground.
 //    Connect the four pins from the ST-Link V2 as shown below:
 //
-//                                                        ST-Link V2 
-//                                 STM32F030F4xx         .-------.     
-//                                 .---. .---.           |   GND |-------.
-//                           BOOT0 |1   V  20| SWCLK --- | SWCLK |       |
-//                             PF0 |2      19| SWCLK --- | SWDIO |       |
-//                             PF1 |3      18| PA10      |  3.3V |---.   |
-//                            NRST |4      17| PA9       '-------'   |   |
-//                            VDDA |5      16| VCC -------- VCC -----'   |
-//    GND---[1K]---[-LED+] --- PA0 |6      15| GND -------- GND ---------'
-//                             PA1 |7      14| PB1
-//                             PA2 |8      13| PA7
-//                             PA3 |9      13| PA6
-//                             PA4 |10     11| PA5
-//                                 '---------'
+//                                                            ST-Link V2 
+//                                    STM32F030F4xx           ╭───────╮     
+//                                     ╭────╮╭────╮           │    GND├───────╮
+//                               BOOT0 │1       20│ SWCLK ────┤SWCLK  │       │
+//                                 PF0 │2       19│ SWCLK ────┤SWDIO  │       │
+//                                 PF1 │3       18│ PA10      │   3.3V├───╮   │
+//                                NRST │4       17│ PA9       ╰───────╯   │   │
+//                                VDDA │5       16│ VCC ──────── VCC ─────╯   │
+//    GND ─── [1K] ─── [-LED+] ─── PA0 │6       15│ GND ──────── GND ─────────╯
+//                                 PA1 │7       14│ PB1
+//                                 PA2 │8       13│ PA7
+//                                 PA3 │9       12│ PA6
+//                                 PA4 │10      11│ PA5
+//                                     ╰──────────╯
 //
 //  ==========================================================================================
 
